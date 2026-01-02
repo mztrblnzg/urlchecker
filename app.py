aa637754b85e2f4ca1346c41d081e030fb4e5c8b
mztrblnzg <dmitryhardlove@gmail.com>
2026-01-02 16:26:47 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
-   from fastapi import FastAPI
-   from fastapi.responses import FileResponse
-   from pydantic import BaseModel
-   from typing import Literal
-   import os
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi import Response
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from typing import Literal
+from fastapi.staticfiles import StaticFiles
+import os
 
-   print("LOADED app.py VERSION=2026-01-02")
+print("LOADED app.py VERSION=2026-01-02")
 
-   app = FastAPI()
+app = FastAPI()
 
-   WHITELIST = {
+@app.get("/", response_class=HTMLResponse)
+    def root():
+        with open("index.html") as f:
+            return HTMLResponse(content=f.read(), status_code=20
+                                
+WHITELIST = {
        "https://urlchecker-n49c.onrender.com",
    }
 
-   class URLCheckRequest(BaseModel):
-       url: str
+class URLCheckRequest(BaseModel):
+    url: str
 
-   @app.get("/")
-   def root():
-       return FileResponse("index.html")
+@app.get("/")
+def root():
+    return FileResponse("index.html")
 
-   @app.get("/health")
-   def health():
-       return {"status": "ok"}
+@app.get("/health")
+def health():
+    return {"status": "ok"}
 
-   @app.post("/check")
-   def check_url(data: URLCheckRequest):
-       status: Literal["ok", "blocked"] = "ok" if data.url in WHITELIST else "blocked"
-       return {"status": status}
+@app.post("/check")
+def check_url(data: URLCheckRequest):
+    status: Literal["ok", "blocked"] = "ok" if data.url in WHITELIST else "blocked"
+    return {"status": status}
